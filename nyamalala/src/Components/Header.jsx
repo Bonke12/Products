d98afdea1123a68a1788afc963820f04d09bd689
Bonke12 <bonkenyamalala39@gmail.com>
2023-05-17 11:55:39 +0200

    add contact on nav

--- nyamalala/src/Components/Header.jsx
+++ nyamalala/src/Components/Header.jsx
@@ -15,6 +15,7 @@ function Header() {
           <Nav className="me-auto">
             <Nav.Link href="#">Home</Nav.Link>
             <Nav.Link href="#">About</Nav.Link>
+            <Nav.Link href="#">Contact</Nav.Link>
             <Nav.Link href="#">Login</Nav.Link>
           </Nav>
           
