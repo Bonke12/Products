af98c8a4338c8d47734ede0015b53bafeb6803ae
Bonke12 <bonkenyamalala39@gmail.com>
2023-05-17 10:31:33 +0200

    I remove common

--- nyamalala/src/Components/Header.jsx
+++ nyamalala/src/Components/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="">Home</Nav.Link>
+            <Nav.Link href="#">Home</Nav.Link>
             <Nav.Link href="#">About</Nav.Link>
             <Nav.Link href="#">Login</Nav.Link>
           </Nav>
