ba9f9d37736bb1f9437cd08807618153d6400590
Bonke12 <bonkenyamalala39@gmail.com>
2023-05-18 11:28:59 +0200

    add header Online

--- nyamalala/src/Components/Header.jsx
+++ nyamalala/src/Components/Header.jsx
@@ -16,6 +16,7 @@ function Header() {
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="about">About</Nav.Link>
             <Nav.Link href="contact">Contact</Nav.Link>
+            <Nav.Link href="contact">Online </Nav.Link>
             <Nav.Link href="login">Login</Nav.Link>
           </Nav>
           
