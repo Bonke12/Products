64a3d6be7f02b3716d5493d99a2dce620c06673d
Bonke12 <bonkenyamalala39@gmail.com>
2023-05-18 15:17:53 +0200

    I copy code from w3school

--- nyamalala/src/Components/Header.jsx
+++ nyamalala/src/Components/Header.jsx
@@ -7,24 +7,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Header() {
   return (
     <>
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#"><h3>Giant African Snail</h3></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="about">About</Nav.Link>
-            <Nav.Link href="contact">Contacts</Nav.Link>
-            {/* <Nav.Link href="contact">Online </Nav.Link> */}
-            <Nav.Link href="login">Sign In</Nav.Link>
-            <a class="btn btn-bd-download d-none d-lg-inline-block mb-3 mb-md-0 ml-md-3" href="#">Download</a>
-          </Nav>
-          <br></br>
-          <i class="bi bi-cart3"></i>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#"><h3>Giant African Snail</h3></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="about">About</Nav.Link>
+              <Nav.Link href="contact">Contacts</Nav.Link>
+              {/* <Nav.Link href="contact">Online </Nav.Link> */}
+              <Nav.Link href="login">Sign In</Nav.Link>
+              {/* <a class="btn btn-bd-download d-none d-lg-inline-block mb-3 mb-md-0 ml-md-3" href="#">Download</a> */}
+            </Nav>
+            <br></br>
+            <i class="bi bi-cart3"></i>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
